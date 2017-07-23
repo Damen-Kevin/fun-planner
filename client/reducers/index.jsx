900a0788723c79d2123a8c29e74afd0552a74aef
Kevin <kjs2b@virginia.edu>
2017-07-23 16:45:35 -0700

    changed onClick method for Activity to a new aaction, display_activity_info (just a console log at the moment)

--- client/reducers/index.jsx
+++ client/reducers/index.jsx
@@ -1,9 +1,11 @@
 
 import { combineReducers } from 'redux';
 import ActivityList from './activity_list';
+import ActivityInfo from './activity_info';
 
 const rootReducer = combineReducers({
-    activity_list: ActivityList
+    activity_list: ActivityList,
+    activity_info: ActivityInfo
 });
 
 export default rootReducer;
